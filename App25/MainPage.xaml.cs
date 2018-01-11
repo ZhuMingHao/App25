b4112cff71f95e9f53a0150a3bbb008d28f0c548
Nico Zhu <v-zhumin@microsoft.com>
2018-01-11 15:57:08 +0800

    Add project files.

---
+++ App25/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App25
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //From Address
+                string FromAddress = "xxxxxxx";
+                string FromAdressTitle = "Expenses to pay";
+                //To Address
+                string ToAddress = "rene.vucko@gmail.com";
+                string ToAdressTitle = "Microsoft ASP.NET Core";
+                string Subject = "Hello World - Sending email using ASP.NET Core 1.1";
+                string BodyContent = "ASP.NET Core was previously called ASP.NET 5. It was renamed in January 2016. It supports cross-platform frameworks ( Windows, Linux, Mac ) for building modern cloud-based internet-connected applications like IOT, web apps, and mobile back-end.";
+
+                //Smtp Server
+                string SmtpServer = "smtp.gmail.com";
+                //Smtp Port Number
+                int SmtpPortNumber = 587;
+
+                var mimeMessage = new MimeMessage();
+                mimeMessage.From.Add(new MailboxAddress(FromAdressTitle, FromAddress));
+                mimeMessage.To.Add(new MailboxAddress(ToAdressTitle, ToAddress));
+                mimeMessage.Subject = Subject;
+                mimeMessage.Body = new TextPart("plain")
+                {
+                    Text = BodyContent
+                };
+
+                using (var client2 = new SmtpClient())
+                {
+                    client2.Connect(SmtpServer, SmtpPortNumber, false);
+                    // Note: only needed if the SMTP server requires authentication
+                    // Error 5.5.1 Authentication
+                    client2.Authenticate("xxxxxx@gmail.com", "xxxxxx");
+                    client2.Send(mimeMessage);
+                    Console.WriteLine("The mail has been sent successfully !!");
+                    Console.ReadLine();
+                    client2.Disconnect(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
